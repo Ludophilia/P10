712e96ff423eb69c550a11c14c94a7de77949a52
Ludophilia <nyaritakunai@outlook.com>
2020-12-25 15:47:42 +0100

    test

---
+++ website/exp.py
@@ -0,0 +1,32 @@
+# for _ in range(10):
+#     if _%2 == 1: continue
+#     print(_)
+#     print(f"WOW {_}")
+
+# def example(message: str) -> None:
+#     print(message)
+
+# example("Salut")
+
+# d = dict(pika = "chu", draco= "feu")
+# print(d.get("rept", ""))
+
+# try:
+#     4/0
+# except:
+#     pass
+# print("lol")
+
+# z = dict(pika="chuu", sala="mèche")
+
+# a = {"rai": "chu", **z}
+
+# print(a)
+
+_, z = (1, 3)
+print(_)
+print(z)
+
+# l = [a+=[x] for x in [1,1,1,1,1,2]]
+# print(l)
+# print([[x] for x in range(10) for y in range(10)])
